9485638d67b41d2ec3b6d939af0f2cd0fdca8ff6
ricky <rickkkkkkkkky@foxmail.com>
2020-01-06 23:11:13 +0800

    refactor: source code link and layout style

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
 import React from 'react'
-import './App.css'
+import './App.scss'
 import Minesweeper from './containers/minesweeper'
 
 const App: React.FC = () => {
   return (
     <div className='app'>
-      <Minesweeper />
+      <div className='app__content'>
+        <Minesweeper />
+      </div>
+      <div className='app__footer'>
+        <a
+          href='https://github.com/rickkky/emoji-minesweeper--react'
+          rel='noopener noreferrer'
+          target='_blank'
+        >
+          <span role='img' aria-label='nerd'>
+            🤓
+          </span>
+          &nbsp; SOURCE CODE &nbsp;
+          <span role='img' aria-label='nerd'>
+            🤓
+          </span>
+        </a>
+      </div>
     </div>
   )
 }
