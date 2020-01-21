92e1b6710465dbbc237e8fd95709384af87c1887
ricky <rickkkkkkkkky@foxmail.com>
2020-01-21 11:48:11 +0800

    chore: delete upper case folder

--- src/containers/MineSweeper/constants.ts
+++
@@ -1,3 +0,0 @@
-export const classBlock = 'minesweeper'
-
-export const numbers = ['', '1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣']
--- src/containers/MineSweeper/index.ts
+++
@@ -1,4 +0,0 @@
-import Container from './Container'
-import './styles/index.scss'
-
-export default Container
